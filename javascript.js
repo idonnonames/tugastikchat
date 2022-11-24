5c13e97cc54e81de35a0c19f995cd265d787ad7c
idonnonames <64767520+idonnonames@users.noreply.github.com>
2022-11-24 22:58:12 +0700

    test js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+var user = cookie.get('user');
+if (!user) {
+
+  // Ask for the username if there is none set already
+  user = prompt('Choose a username:');
+  if (!user) {
+    alert('We cannot work with you like that!');
+  } else {
+    // Store it in the cookies for future use
+    cookie.set('user', user);
+  }
+}
